c8b0d80f65f737f024a5e28ac00bf2752a00baa9
andrewkelley <akelle17@kent.edu>
2013-10-27 20:21:28 -0400

    garagedoor action works

--- routes/garagedoor.js
+++ routes/garagedoor.js
@@ -1,13 +1,23 @@
 
 /* open or close garage door */
 var gpio = require("pi-gpio");
+var toggle = 0;
 
 exports.action = function(req, res){
-  gpio.open(7, "output", function(err) {    // Open pin 7 for output
-    gpio.write(7, 1, function() {           // Set pin 7 high (1)
-      gpio.close(7);                        // Close pin 7
+  if (toggle) {
+    gpio.open(7, "output", function(err) {    // Open pin 7 for output
+      gpio.write(7, 1, function() {           // Set pin 7 high (1)
+        gpio.close(7);                        // Close pin 7
+      });
     });
-  });
+  } else {
+    gpio.open(7, "output", function(err) {    // Open pin 7 for output
+      gpio.write(7, 0, function() {           // Set pin 7 low (0)
+        gpio.close(7);                        // Close pin 7
+      });
+    });
+  } 
+  toggle = !toggle;
   res.send("success"); 
 };
 
